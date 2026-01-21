fdadb21fa16e9bff347f307d1fd6891aaeb1d797
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:23:43 +0100

    Add Collection Primitives dependency
    Queues are collections and should depend on Collection Primitives for
    collection view types and transformations.

--- Package.swift
+++ Package.swift
@@ -18,13 +18,19 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../swift-list-primitives")
+        .package(path: "../swift-list-primitives"),
+        .package(path: "../swift-index-primitives"),
+        .package(path: "../swift-input-primitives"),
+        .package(path: "../swift-collection-primitives"),
     ],
     targets: [
         .target(
             name: "Queue Primitives",
             dependencies: [
-                .product(name: "List Primitives", package: "swift-list-primitives")
+                .product(name: "List Primitives", package: "swift-list-primitives"),
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
+                .product(name: "Input Primitives", package: "swift-input-primitives"),
+                .product(name: "Collection Primitives", package: "swift-collection-primitives"),
             ]
         ),
         .testTarget(
